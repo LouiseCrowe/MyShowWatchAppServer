c1135ad0ae9f554bdc8e99f2d434a8a4ab44675f
LouiseCrowe <louise.crowe7@gmail.com>
2020-12-19 11:21:39 +0000

    Published Code

--- obj/Debug/netcoreapp3.1/MyShowWatch.Server.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/MyShowWatch.Server.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("88d5ebbf-7522-4cf6-a975-c87ffb998c4e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShowWatch.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
